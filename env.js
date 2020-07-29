e818b2ab4d6ee34080ef446e80e508ca950251cb
Emanuel Lauria <emanuel.lauria@pm.me>
2020-07-29 12:58:15 +0300

    gitignore

--- env.js
+++
@@ -1,8 +0,0 @@
-const url = 'http://localhost:9999'
-const org = 'rifecorp'
-const bucket = 'ohlcv'
-const token = 'iN7hUlMvsDQdruqkhKSdkntzn6RbBkXZCOK37TXz7lYMzciZZLoNeCNLhYBDR4zPMt3dqKax6D1sWviBqQdqEA=='
-const listedExchanges = ['bitmex', 'bitfinex']
-module.exports = {
-	url, org, bucket, token, listedExchanges
-};
